8a0f02223d8516f26c2a75cf4a9582666e4b8231
Sathya <neelasathya06@gmail.com>
2025-05-26 20:13:04 +0530

    responsive designs implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className="bg-white-100 flex justify-center items-center h-screen">
-       <div className='bg-white p-8 rounded-lg shadow-lg'>
-           <h1 className="text-2xl font-bold text-gray-500 flex justify-center">Hello, Tailwind CSS in react!</h1>
-           <p className='text-gray-600'>
-               This is a simple example using Tailwind CSS in React Project!
-           </p>
-       </div>
+   <div className="bg-gray-100 p-4">
+       <ul className='list-none md:flex md:gap-4'>
+           <li className="bg-blue-500 m-2 text-white p-2 rounded-lg md:px-4">Item 1</li>
+           <li className="bg-blue-500 m-2 text-white p-2 rounded-lg">Item 2</li>
+           <li className="bg-blue-500 m-2 text-white p-2 rounded-lg">Item 3</li>
+       </ul>
    </div>
   )
 }
